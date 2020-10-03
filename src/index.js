ea0a6ec91627cff6e433e599525ab1c907b9be0b
Dmitry Gutkovsky <femiarkh@gmail.com>
2020-10-03 08:28:59 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const charNumber = expr.length / 10;
+    const arr = [];
+    for (let i = 0; i < charNumber; i++) {
+      const start = i * 10;
+      const end = (i + 1) * 10;
+      const subStr = expr.substring(start, end);
+      arr.push(subStr);
+    }
+    const result = [];
+    arr.forEach((codedChar) => {
+      const charCodeArr = [];
+      for (let i = 8; i >= 0; i -= 2) {
+        const codeNumber = codedChar.substring(i, i + 2);
+        if (codeNumber === '10') {
+          charCodeArr.unshift('.');
+        } else if (codeNumber === '11') {
+          charCodeArr.unshift('-');
+        } else if (codeNumber === '**') {
+          charCodeArr.unshift(' ');
+          break;
+        } else {
+          break;
+        }
+      }
+      const morseCodedChar = charCodeArr.join('');
+      if (morseCodedChar === ' ') {
+        result.push(' ');
+      } else {
+        result.push(MORSE_TABLE[morseCodedChar]);
+      }
+    });
+    return(result.join(''));
 }
 
 module.exports = {
